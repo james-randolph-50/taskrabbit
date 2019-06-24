8db81b817fcbe42f696a67b337e7320f5fc8122a
Mary Kate <MaryKate@Marys-MacBook-Pro-3.local>
2019-06-24 14:48:15 -0400

    add categories, edit/delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,12 @@ class TasksController < ApplicationController
     before_action :find_task, only: [:show, :edit, :update, :destroy]
 
     def index
+        if params[:category].blank?
+            @tasks = Task.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @tasks = Task.where(category_id: @category_id).order("created_at DESC")
+        end
     end
 
     def show
@@ -22,18 +28,26 @@ class TasksController < ApplicationController
     end
 
     def edit
+
     end
 
     def update
+        if @task.update(tasks_params)
+            redirect_to @task
+        else
+            render "Edit"
+        end
     end
 
     def destroy
+        @task.destroy
+        redirect_to root_path
     end
 
     private
 
     def tasks_params
-        params.require(:task).permit(:title, :description, :company, :url)
+        params.require(:task).permit(:title, :description, :company, :url, :category_id)
     end
 
     def find_task
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_174211) do
+ActiveRecord::Schema.define(version: 2019_06_24_183255) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_22_174211) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
